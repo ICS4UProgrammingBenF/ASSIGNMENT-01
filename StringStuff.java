ff0ec94530ed533478d5993ae501960a22cbdcfc
bfalsetto <benjamin.falsetto@ocsbstudent.ca>
2020-09-27 16:21:01 +0000

    Completed first half of main, started convertchar()

--- StringStuff.java
+++ StringStuff.java
@@ -1,8 +1,33 @@
+//import necessary classes
+import java.util.Scanner;
+
 /**
  * Class StringStuff
  */
 public class StringStuff {
-	public static void main(String[] args) {
-    	System.out.println("Hello from AWS Cloud9!");
-	}
+  
+  /**
+   * Required Javadoc.
+  */
+  
+  /**
+   * Creating the convertchar() function
+  */
+  
+  public static String convertchar(String userInput)  {
+    charToParse[] = 
+  }
+  
+  public static void main(String[] args) {
+    
+    //declare variables
+    String output = "";
+    //String userInput = "";
+    
+    //get the input from the user
+    Scanner input = new Scanner(System.in);
+    String userInput = input.next();
+    
+    System.out.println(userInput);
+  }
 }
