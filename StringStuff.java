199d1016bf95a2f912638a7e8ee448615cbcbca6
bfalsetto <benjamin.falsetto@ocsbstudent.ca>
2020-09-29 20:29:35 +0000

    completed most of the code. All done except reading from and writing to files

--- StringStuff.java
+++ StringStuff.java
@@ -1,4 +1,5 @@
 //import necessary classes
+import java.io*;
 import java.util.Scanner;
 
 /**
@@ -7,16 +8,73 @@ import java.util.Scanner;
 public class StringStuff {
   
   /**
-   * Required Javadoc.
+   * Creating the blowup() function
   */
+  public static String blowup(String userInput)  {
+    
+    //String charAti = "";
+    //String charAfteri = "";
+    String output = "";
+    boolean endOfArray = false;
+    
+    //putting string into array
+    char charArray[] = userInput.toCharArray();
+    
+    //looping through each char
+    for (int i = 0; i < charArray.length ; i++)  {
+      
+      //getting the char at index i to check
+      String charAti = Character.toString(charArray[i]);
+      
+      //check to see if char at index i is a digit
+      boolean isDigit = isdigit(charAti);
+      
+      if (isDigit != true) {
+        //if not a digit, output to the console
+        output += charAti;
+      } else  {
+        //check to see if char at index i is the last char
+        endOfArray = endofarray(charArray, i);
+        
+        if (endOfArray != true) {
+          //get the numeric value of char at i
+          int intAti = Integer.parseInt(charAti);
+          String charAfteri = Character.toString(charArray[i + 1]);
+          for (int repeater = 0; repeater < intAti; repeater++)  {
+            output += charAfteri;
+          }
+        } else  {
+          //if true. do nothing
+        }
+      }
+    }
+    
+    return output;
+  }
   
   /**
-   * Creating the convertchar() function
+   * Testing if char at index i is a digit.
   */
+  public static boolean isdigit(String charAti) {
+    try {
+      Integer.parseInt(charAti);
+      return true;
+    } catch(NumberFormatException e) {
+      return false;
+    }
+  }
   
-  public static String blowup(String userInput)  {
-    char[] charArray = userInput.toCharArray();
-    return userInput;
+  /**
+   * Testing if char at index i is the last char.
+  */
+  public static boolean endofarray(char charArray[], int i) {
+    try {
+      //checking each i
+      String charAfteri = Character.toString(charArray[i + 1]);
+      return false;
+    } catch(ArrayIndexOutOfBoundsException e) {
+      return true;
+    }
   }
   
   public static void main(String[] args) {
@@ -25,13 +83,15 @@ public class StringStuff {
     String output = "";
     //String userInput = "";
     
+    //ask the user for an input
+    System.out.println("Smash something against the keyboard");
+    
     //get the input from the user
     Scanner input = new Scanner(System.in);
     String userInput = input.next();
     
     String testVal = blowup(userInput);
     
-    System.out.println(userInput);
     System.out.println(testVal);
   }
 }
